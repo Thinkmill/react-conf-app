4a4e0a98287cf8db6cfceda9dad9f834af2df95e
Joss Mackison <joss@thinkmill.com.au>
2017-02-23 22:12:27 +1100

    put some love into the "Now Button" on schedule

--- app/scenes/Schedule/components/NowButton/index.js
+++ app/scenes/Schedule/components/NowButton/index.js
@@ -1,25 +1,63 @@
 import React, { Component, PropTypes } from 'react';
-import { StyleSheet, TouchableHighlight, Text, View } from 'react-native';
+import { Animated, StyleSheet, TouchableWithoutFeedback, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import LinearGradient from 'react-native-linear-gradient';
 
 import theme from '../../../../theme';
-import { darken } from '../../../../utils/color';
+import { lighten, darken } from '../../../../utils/color';
 
 export default class NowButton extends Component {
+	constructor (props) {
+		super(props);
+
+		this.state = { animValue: new Animated.Value(0) };
+	}
+	springToValue (val) {
+		Animated.spring(this.state.animValue, {
+			toValue: val,
+			friction: 3,
+			tension: 50,
+		}).start();
+	}
 	render () {
 		const { onPress } = this.props;
+		const { animValue } = this.state;
+		const { green } = theme.color;
+
+		const gradientColors = [lighten(green, 6), darken(green, 6)];
 		const touchableProps = {
-			activeOpacity: 1,
+			hitSlop: {
+				bottom: theme.fontSize.large,
+				left: 10,
+				right: 10,
+				top: 10,
+			},
 			onPress,
-			style: styles.button,
-			underlayColor: darken(theme.color.green, 10),
+			onPressIn: () => this.springToValue(1),
+			onPressOut: () => this.springToValue(0),
+		};
+		const dynamicStyles = {
+			transform: [{
+				scale: animValue.interpolate({
+					inputRange: [0, 1],
+					outputRange: [1, 0.9],
+				}),
+			}],
 		};
 
 		return (
-			<View style={styles.layout}>
-				<TouchableHighlight {...touchableProps}>
-					<Text style={styles.text}>NOW</Text>
-				</TouchableHighlight>
-			</View>
+			<Animated.View style={[styles.layout, dynamicStyles]}>
+				<TouchableWithoutFeedback {...touchableProps}>
+					<LinearGradient colors={gradientColors} style={styles.button}>
+						<Icon
+							color="white"
+							name="ios-timer"
+							size={32}
+							style={{ backgroundColor: 'transparent', height: 32 }}
+						/>
+					</LinearGradient>
+				</TouchableWithoutFeedback>
+			</Animated.View>
 		);
 	}
 };
@@ -29,19 +67,19 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		bottom: 0,
 		left: 0,
-		paddingBottom: 10,
+		paddingBottom: theme.fontSize.large,
 		position: 'absolute',
 		right: 0,
 	},
 	button: {
-		justifyContent: 'center',
 		backgroundColor: theme.color.green,
-		borderRadius: 34,
-		height: 34,
-		paddingHorizontal: 24,
+		borderRadius: 40,
+		height: 40,
+		justifyContent: 'center',
+		paddingHorizontal: 32,
 		shadowColor: 'black',
 		shadowOffset: { height: 1, width: 0 },
-		shadowOpacity: 0.25,
+		shadowOpacity: 0.24,
 		shadowRadius: 2,
 	},
 	text: {

525804893fb222e56e1653d9a38e5f5df62a2331
alejandro garcia <agrcrobles@gmail.com>
2017-03-08 21:52:40 +0100

    Fixed android app #41

--- index.android.js
+++ index.android.js
@@ -2,4 +2,4 @@
 import ReactConf2017 from './app/ReactConf2017';
 import { AppRegistry } from 'react-native';
 
-AppRegistry.registerComponent('ReactConf2017', () => ReactConf2017);
+AppRegistry.registerComponent('app', () => ReactConf2017);

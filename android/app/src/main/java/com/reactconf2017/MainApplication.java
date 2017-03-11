316c0ed603ec5202c5d78c9b6ee5e9f36c3449e3
Kevin Brown <kevin.brown@thinkmill.com.au>
2017-03-11 16:40:47 +1100

    Added code push deployment key for Android.

--- android/app/src/main/java/com/reactconf2017/MainApplication.java
+++ android/app/src/main/java/com/reactconf2017/MainApplication.java
@@ -40,7 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
             new RCTSplashScreenPackage(),
             new LinearGradientPackage(),
             new MapsPackage(),
-            new CodePush(null, getApplicationContext(), BuildConfig.DEBUG),
+            new CodePush("_E6QzaZjF08AWXt-43EdcgDhSg9M4JJQBoKGz", getApplicationContext(), BuildConfig.DEBUG),
             new BlurViewPackage(),
             new VectorIconsPackage()
       );

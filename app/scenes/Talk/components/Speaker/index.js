3fe853390aa9c2c2ec0f637e1695fd71af35c6c9
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2018-03-13 09:57:32 +0100

    WIP

--- app/scenes/Talk/components/Speaker/index.js
+++ app/scenes/Talk/components/Speaker/index.js
@@ -1,33 +1,32 @@
-// 
-import React, { Component } from 'react';
+//
+import React, { Component } from "react";
 import {
   PixelRatio,
   StyleSheet,
   Text,
   TouchableHighlight,
   TouchableOpacity,
-  View,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+  View
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
-import Avatar from '../../../../components/Avatar';
-import DraggableView from '../../../../components/DraggableView';
-import Modal from '../../../../components/Modal';
-import theme from '../../../../theme';
-import { attemptToOpenUrl } from '../../../../utils';
-
+import Avatar from "../../../../components/Avatar";
+import DraggableView from "../../../../components/DraggableView";
+import Modal from "../../../../components/Modal";
+import theme from "../../../../theme";
+import { attemptToOpenUrl } from "../../../../utils";
 
 function Button({ bordered, icon, onPress, text }) {
   const touchableProps = {
     activeOpacity: 1,
     onPress: onPress,
     style: styles.buttonTouchable,
-    underlayColor: theme.color.gray05,
+    underlayColor: theme.color.gray05
   };
 
   const dynamicStyles = {
     borderLeftColor: bordered ? theme.color.gray20 : null,
-    borderLeftWidth: bordered ? 1 / PixelRatio.get() : null,
+    borderLeftWidth: bordered ? 1 / PixelRatio.get() : null
   };
 
   return (
@@ -41,23 +40,15 @@ function Button({ bordered, icon, onPress, text }) {
 }
 
 export default class Speaker extends Component {
-
   static defaultProps = {
-    onPress() {},
+    onPress() {}
   };
 
   handleClose = () => {
     this.refs.modal.onClose();
   };
   render() {
-    const {
-      avatar,
-      github,
-      name,
-      onClose,
-      summary,
-      twitter,
-    } = this.props;
+    const { avatar, github, name, onClose, summary, twitter } = this.props;
     const showButtons = !!(github || twitter);
 
     // @jossmac Re: DraggableView - onRelease()
@@ -78,36 +69,41 @@ export default class Speaker extends Component {
               onPress={this.handleClose}
               activeOpacity={0.5}
               style={{
-                position: 'absolute',
+                position: "absolute",
                 top: 0,
                 right: 0,
                 height: 44,
                 width: 44,
-                alignItems: 'center',
-                justifyContent: 'center',
+                alignItems: "center",
+                justifyContent: "center"
               }}
             >
               <Icon color={theme.color.gray40} name="md-close" size={24} />
             </TouchableOpacity>
           </View>
-          {showButtons &&
+          {showButtons && (
             <View style={styles.buttons}>
-              {!!twitter &&
+              {!!twitter && (
                 <Button
                   icon="logo-twitter"
                   onPress={() =>
-                    attemptToOpenUrl('https://twitter.com/' + twitter)}
-                  text={'@' + twitter}
-                />}
-              {!!github &&
+                    attemptToOpenUrl("https://twitter.com/" + twitter)
+                  }
+                  text={"@" + twitter}
+                />
+              )}
+              {!!github && (
                 <Button
                   bordered
                   icon="logo-github"
                   onPress={() =>
-                    attemptToOpenUrl('https://github.com/' + github)}
+                    attemptToOpenUrl("https://github.com/" + github)
+                  }
                   text={github}
-                />}
-            </View>}
+                />
+              )}
+            </View>
+          )}
         </DraggableView>
       </Modal>
     );
@@ -116,52 +112,51 @@ export default class Speaker extends Component {
 
 const styles = StyleSheet.create({
   wrapper: {
-    backgroundColor: 'white',
-    shadowColor: 'black',
+    backgroundColor: "white",
+    shadowColor: "black",
     shadowOffset: { height: 1, width: 0 },
     shadowOpacity: 0.25,
-    shadowRadius: 5,
+    shadowRadius: 5
   },
 
   // main
   main: {
-    alignItems: 'center',
-    padding: theme.fontSize.large,
+    alignItems: "center",
+    padding: theme.fontSize.large
   },
   mainTitle: {
     color: theme.color.text,
     fontSize: theme.fontSize.large,
-    fontWeight: '300',
-    marginVertical: theme.fontSize.default,
+    fontWeight: "300",
+    marginVertical: theme.fontSize.default
   },
   mainText: {
     color: theme.color.text,
     fontSize: 15,
-    fontWeight: '300',
+    fontWeight: "300",
     lineHeight: 21,
-    textAlign: 'center',
-    width: '80%',
+    textAlign: "center"
   },
 
   // buttons
   buttons: {
-    overflow: 'hidden',
-    flexDirection: 'row',
+    overflow: "hidden",
+    flexDirection: "row"
   },
   buttonTouchable: {
-    backgroundColor: 'white',
-    flex: 1,
+    backgroundColor: "white",
+    flex: 1
   },
   button: {
-    alignItems: 'center',
+    alignItems: "center",
     borderTopColor: theme.color.gray20,
     borderTopWidth: 1 / PixelRatio.get(),
-    paddingVertical: theme.fontSize.large,
+    paddingVertical: theme.fontSize.large
   },
   buttonIcon: {
-    color: theme.color.blue,
+    color: theme.color.blue
   },
   buttonText: {
-    color: theme.color.gray60,
-  },
+    color: theme.color.gray60
+  }
 });

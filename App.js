ac34e7ea7cf82d57b8ed3718cee86dd2171ab834
Manuel Lehner <manuel.lehner@sandstorm.de>
2018-03-16 23:51:07 +0100

    [TASK] update assets + minor android adjusting

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ReactConf2017 from './app/ReactConf2017';
+import NeosConApp from './app/NeosCon';
 
-export default ReactConf2017;
+export default NeosConApp;
